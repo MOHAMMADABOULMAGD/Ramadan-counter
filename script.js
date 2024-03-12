51070092340e3a65d0cf63a631a8d6572144ce9d
MOHAMMADABOULMAGD <134284260+MOHAMMADABOULMAGD@users.noreply.github.com>
2024-03-12 15:12:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var ramadanStartDate = new Date("March 11, 2024").getTime();
 function updateCountdown() {
     var now = new Date().getTime();
     var timePassed = now - ramadanStartDate;
-    
+
     if (timePassed >= 30 * 24 * 60 * 60 * 1000) {
         // إذا مر أكثر من 30 يومًا، قم بتغيير النص إلى انتهى رمضان منذ
         var countdownElement = document.getElementById("countdown");
@@ -14,11 +14,12 @@ function updateCountdown() {
     }
 
     var timeRemaining = ramadanStartDate - now;
+    var absoluteTimeRemaining = Math.abs(timeRemaining);  
 
-    var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    var days = Math.floor(absoluteTimeRemaining / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((absoluteTimeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((absoluteTimeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((absoluteTimeRemaining % (1000 * 60)) / 1000);
 
     var daysText = "يوم";
 
