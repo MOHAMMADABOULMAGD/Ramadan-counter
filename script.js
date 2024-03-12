c152989194014e68af9746033919d6fa8bbeb7d6
MOHAMMADABOULMAGD <134284260+MOHAMMADABOULMAGD@users.noreply.github.com>
2024-03-12 14:59:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ var ramadanStartDate = new Date("March 11, 2024").getTime();
 function updateCountdown() {
     var now = new Date().getTime();
     var timePassed = now - ramadanStartDate;
-    var absoluteTimePassed = Math.abs(timePassed);
 
-    if (absoluteTimePassed >= 30 * 24 * 60 * 60 * 1000) {
+    if (timePassed >= 30 * 24 * 60 * 60 * 1000) {
         // إذا مر أكثر من 30 يومًا، قم بتغيير النص إلى انتهى رمضان منذ
         var countdownElement = document.getElementById("countdown");
         countdownElement.innerHTML = "انتهى رمضان منذ";
@@ -16,14 +15,31 @@ function updateCountdown() {
 
     var timeRemaining = ramadanStartDate - now;
 
-    var days = Math.floor(absoluteTimePassed / (1000 * 60 * 60 * 24));
+    var days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
     var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
+    var daysText = "يوم";
+
+    var hoursText = "ساعة";
+    if (hours > 3 && hours < 10) {
+        hoursText = "ساعات";
+    }
+
+    var minutesText = "دقيقة";
+    if (minutes > 3 && minutes < 11) {
+        minutesText = "دقائق";
+    }
+
+    var secondsText = "ثوانٍ";
+    if (seconds > 10 && seconds <= 60) {
+        secondsText = "ثانية";
+    }
+
     var countdownElement = document.getElementById("countdown");
-    var countdownPrefix = (timePassed >= 0) ? "بدأ رمضان" : "باقي على رمضان";
-    countdownElement.innerHTML = countdownPrefix;
+    var countdownPrefix = (timePassed >= 0) ? "بدأ رمضان منذ" : "باقي على رمضان";
+    countdownElement.innerHTML = countdownPrefix + " " + days + " " + daysText + " و " + hours + " " + hoursText + " و " + minutes + " " + minutesText + " و " + seconds + " " + secondsText;
 
     // التحديث كل ثانية
     setTimeout(updateCountdown, 1000);
